77138fb8b163bd6a1c0be074fb38fabeac741b44
LeeMck <leemck@gmail.com>
2019-07-18 16:04:46 -0700

    Add files via upload

--- co2readstart.sh
+++ co2readstart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd co2
 # Script to start the current CO2 meter program 
+# Small change to see how github takes it.
 echo "Now in co2 python instrument subdirectory"
 stty -F /dev/ttyUSB0 speed 9600 cs7
 stty -F /dev/ttyUSB1 speed 9600 cs7
